040e6b07479a1780a169b317f20b3ef7217160ad
Eduardo <131834306+EduardoR22@users.noreply.github.com>
2024-01-18 11:51:48 -0600

    Error in the nav was corrected so that when you click on the link it closes. The default image was added in reviews when the user does not have an image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import styles from '@/app/sass/layout/_header.module.scss';
 import menu from '@/app/sass/layout/_nav.module.scss';
 import menNav from '@/app/sass/layout/_menav.module.scss';
-import navigation from '@/app/sass/layout/_navigation.module.scss';
-import menBurguer from '@/app/sass/layout/_menuburguer.module.scss';
 import footer from '@/app/sass/layout/_footer.module.scss';
 import vinietamenu from '@/app/sass/layout/vinietamenu.module.scss';
+import NavBurguer from './NavBurguer';
 
 export default function Header(){
+
   return(
     <>
       {/* <img srcSet="next.svg 1x, next.svg 2x" alt="next" /> */}
@@ -21,13 +21,11 @@ export default function Header(){
               className={footer.logobox}
             />
           </a>
-          {/* <img srcSet="next.svg 1x, next.svg 2x" alt="next" /> */}
-          {/* <ul className="navegator navegator__links"> */}
           <ul className={`${menNav.navegator} ${menNav.navegator__links}`}>
             {/* <li className="navegator__item"> */}
-            <li className={menNav.naveburguer__item}>
+            {/* <li className={menNav.naveburguer__item}> */}
+            <li className={menNav.navegator__item}>
               <a
-                // className="navegator__a navegator__a--notshow navegator__a--notshowmini"
                 className={`${menNav.navegator__a} ${menNav.navegator__a__notshow} ${menNav.navegator__a__notshowmini}`}
                 href="/#section-nosotros"
                 >Nosotros
@@ -36,20 +34,17 @@ export default function Header(){
             {/* <li className="navegator__item"> */}
             <li className={menNav.navegator__item}>
               <a
-                //className="navegator__a navegator__a--notshow"
                 className={`${menNav.navegator__a} ${menNav.navegator__a__notshow}`}
                 href="/#section-servicios"
                 >Servicios
               </a>
             </li>
             <li className={menNav.navegator__item}>
-              {/* <a className="navegator__a navegator__a--notshow" href="#section-equipo" */}
               <a className={`${menNav.navegator__a} ${menNav.navegator__a__notshow}`} href="/#section-proyectos"
                 >Proyectos
               </a>
             </li>
             <li className={menNav.navegator__item}>
-              {/* <a className="navegator__a navegator__a--notshow" href="#section-costos" */}
               <a className={`${menNav.navegator__a} ${menNav.navegator__a__notshow}`} href="#section-clientes"
                 >Clientes
               </a>
@@ -63,10 +58,8 @@ export default function Header(){
               </a>
             </li>
           </ul>
-          {/* <div className="vinietamenu__list--phone vinietamenu__list--head">           */}
           <div className={`${vinietamenu.vinietamenu__list__phone} ${vinietamenu.vinietamenu__list__head}`}>
             <a
-              //className="vinietamenu__list--text"
               className={vinietamenu.vinietamenu__list__text}
               href="https://api.whatsapp.com/send?phone=524442422520&text=Hola"
               target="_blank"
@@ -74,36 +67,7 @@ export default function Header(){
             </a>
           </div>
         </nav>
-        <div className={navigation.navigation}>
-          <input type="checkbox" className={navigation.navigation__checkbox} id="navi-toogle" />
-          <label htmlFor="navi-toogle" className={navigation.navigation__button}>
-            <span className={navigation.navegator__icon}>&nbsp;</span>
-          </label>
-          <div className={navigation.navigation__background}>&nbsp;</div>
-          <nav className={`${navigation} ${navigation.navigation__nav}`}>
-            <ul className={`${menBurguer.naveburguer} ${menBurguer.naveburguer__list}`}>
-              <li className={menBurguer.naveburguer__item}>
-                <a className={menBurguer.naveburguer__a} href="#section-nosotros">Nosotros</a>
-              </li>
-              <li className={menBurguer.naveburguer__item}>
-                <a className={menBurguer.naveburguer__a} href="#section-servicios"
-                  >Servicios
-                </a>
-              </li>
-              <li className={menBurguer.naveburguer__item}>
-                <a className={menBurguer.naveburguer__a} href="#section-proyectos">Proyectos</a>
-              </li>
-              <li className={menBurguer.naveburguer__item}>
-                <a className={menBurguer.naveburguer__a} href="#section-clientes">Clientes</a>
-              </li>
-              <li className={menBurguer.naveburguer__item}>
-                <a className={menBurguer.naveburguer__a} href="#section-contactanos"
-                  >Contáctanos
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+        <NavBurguer />
       </header>
     </>
   )
