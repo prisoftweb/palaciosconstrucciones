d0c55f591c618302c2907e56e04c3e9754305954
prisoftweb <prisoft@hotmail.com>
2024-03-07 20:21:50 -0600

    Add analitycs TAG Manager

--- src/components/GTM/index.js
+++ src/components/GTM/index.js
@@ -8,7 +8,7 @@ export const GTMscript = () => (
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-PPTRZ8L9');</Script> */}
 
-        <Script>{`window.dataLayer = window.dataLayer || [];`}</Script>
+        {/* <Script>{`window.dataLayer = window.dataLayer || [];`}</Script>
         <Script id="google-tag-manager" strategy="afterInteractive">
             {`
                 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -17,17 +17,35 @@ export const GTMscript = () => (
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                 })(window,document,'script','dataLayer', 'GTM-PPTRZ8L9')
             `}
-        </Script>
+        </Script> */}
+        
+        <Script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PPTRZ8L9');</Script>
+
     </>
 );
 
 export const GTMnoscript = () => (
-    <noscript
-        dangerouslySetInnerHTML={{
-            __html: `
-                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPTRZ8L9"
-                height="0" width="0" style="display:none;visibility:hidden"></iframe>
-            `,
-        }}
-    />
+    <>
+        <noscript>
+            <iframe 
+                src="https://www.googletagmanager.com/ns.html?id=GTM-PPTRZ8L9"
+                height="0" 
+                width="0" 
+                style="display:none;visibility:hidden">
+            </iframe>
+        </noscript>
+    </>
+
+    // <noscript
+    //     dangerouslySetInnerHTML={{
+    //         __html: `
+    //             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PPTRZ8L9"
+    //             height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    //         `,
+    //     }}
+    // />
 );
